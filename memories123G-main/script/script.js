e8243fa600dbcd16d46bfe306496958ad3dd6ab6
BARGUENABRYAN <114083565+BARGUENABRYAN@users.noreply.github.com>
2024-08-28 08:58:40 -0300

    Add files via upload

---
+++ memories123G-main/script/script.js
@@ -0,0 +1,89 @@
+let entrevista_1 = {
+    entrevistador: "João",
+    entrevistado: "Bryan",
+    questoes: {
+        p1: "Qual seu maior sonho profissional?",
+        r1: "ser patrão do setor administrativo",
+        p2: "Se hoje você pudesse escolher qualquer curso em qualquer lugar, qual e onde seria?",
+        r2: " administração na Unicesumar de maringá",
+        p3: "Se pudesse aprender qualquer habilidade nova, qual seria?",
+        r3: "ler mentes, para saber o que as pessoas pensam",
+        p4: "O que você mais gostou de aprender na escola?",
+        r4: "ficar longe de pessoas falsas",
+        p5: "Onde você se ve daqui 5 anos?",
+        r5: " me vejo casado e com filhos",
+        p6: "Qual legado você gostaria de deixar para seu futuro?",
+        r6: "não deixe para amanhã o que pode fazer hoje",
+        p7: "Se você pudesse deixar um conselho para quem está entrando no ensino médio qual seria?",
+        r7: "irão vim grandes desafios mais nunca desista",
+        p8: "Se você pudesse voltar no tempo e dar um conselho a si mesmo, o que diria?",
+        r8: "se afaste daquela pessoa"
+    }
+}
+let entrevista_2 = {
+    entrevistador: "Lorena",
+    entrevistado: "Vinicius",
+    questoes: {
+        p1: "Qual seu maior sonho profissional?",
+        r1: "me formar em Matematica e trabalhar com isso no mercado financeiro",
+        p2: "Se hoje você pudesse escolher qualquer curso em qualquer lugar, qual e onde seria?",
+        r2: "Cursaria matematica na USP (Universidade de São Paulo)",
+        p3: "Se pudesse aprender qualquer habilidade nova, qual seria?",
+        r3: "voar, porque eu sempre quis voar",
+        p4: "O que você mais gostou de aprender na escola?",
+        r4: "matematica, gostei bastante de aprender trigonometria",
+        p5: "Onde você se ve daqui 5 anos?",
+        r5: " eu me vejo no ultimo ano da faculdade, esperando a formatura.",
+        p6: "Qual legado você gostaria de deixar para seu futuro?",
+        r6: "gostaria de deixar muito dinheiro para meus filhos adotivos",
+        p7: "Se você pudesse deixar um conselho para quem está entrando no ensino médio qual seria?",
+        r7: "estude, mas principalmente aproveite bastante com seus amigos, pois a maior parte deles irão embora.",
+        p8: "Se você pudesse voltar no tempo e dar um conselho a si mesmo, o que diria?",
+        r8: "aproveite e repense em todas as suas atitudes, pois tudo tem uma consequencia"
+    }
+}
+let entrevista_3 = {
+    entrevistador: "Bryan",
+    entrevistado: "Lorena ",
+    questoes: {
+        p1: "Qual seu maior sonho profissional?",
+        r1: "ser veterinaria, e ter uma clinica em meu nome",
+        p2: "Se hoje você pudesse escolher qualquer curso em qualquer lugar, qual e onde seria?",
+        r2: " faria medicina veterinaria na UEM de Umuarama",
+        p3: "Se pudesse aprender qualquer habilidade nova, qual seria?",
+        r3: "saber fazer tatuagem",
+        p4: "O que você mais gostou de aprender na escola?",
+        r4: "a valorizar as pessoas verdadeiras",
+        p5: "Onde você se ve daqui 5 anos?",
+        r5: "",
+        p6: "Qual legado você gostaria de deixar para seu futuro?",
+        r6: "",
+        p7: "Se você pudesse deixar um conselho para quem está entrando no ensino médio qual seria?",
+        r7: "",
+        p8: "Se você pudesse voltar no tempo e dar um conselho a si mesmo, o que diria?",
+        r8: ""
+    }
+}
+let entrevista_4 = {
+    entrevistador: "Vinicius",
+    entrevistado: "João",
+    questoes: {
+        p1: "Qual seu maior sonho profissional?",
+        r1: "",
+        p2: "Se hoje você pudesse escolher qualquer curso em qualquer lugar, qual e onde seria?",
+        r2: "",
+        p3: "Se pudesse aprender qualquer habilidade nova, qual seria?",
+        r3: "",
+        p4: "O que você mais gostou de aprender na escola?",
+        r4: "",
+        p5: "Onde você se ve daqui 5 anos?",
+        r5: "",
+        p6: "Qual legado você gostaria de deixar para seu futuro?",
+        r6: "",
+        p7: "Se você pudesse deixar um conselho para quem está entrando no ensino médio qual seria?",
+        r7: "",
+        p8: "Se você pudesse voltar no tempo e dar um conselho a si mesmo, o que diria?",
+        r8: ""
+    }
+}
+
